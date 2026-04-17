875074a33f78720c6a1642753283a16334a85f44
honey0702 <patelhoney724@gmail.com>
2026-04-17 10:43:46 +0530

    fix homepage

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('home/' , views.home , name='home'),
+      path('', views.home, name='home'),
      path('product/<int:id>/', views.product_detail, name='product_detail'),
      path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
      path('cart/', views.cart, name='cart'),
